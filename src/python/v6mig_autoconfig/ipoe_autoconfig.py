d7d3270c5a2f6f290e312a667ae6c6c4842225f7
Kazuki Shimizu <kazubu@jtime.net>
2022-03-30 11:12:37 +0900

    implemented configuration update and scheduled update

--- src/python/v6mig_autoconfig/ipoe_autoconfig.py
+++ src/python/v6mig_autoconfig/ipoe_autoconfig.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
-from logging import getLogger, Formatter, StreamHandler, DEBUG
+from datetime import datetime
+from logging import getLogger, Formatter, StreamHandler
 import argparse
 import copy
 import collections
 import json
 import os
+import random
 import sys
 import time
 
@@ -39,10 +41,16 @@ MQTT_TIMEOUT = 180
 logger = getLogger(__name__)
 handlers = collections.defaultdict(set)
 
+next_update = None
+provisioned_ttl = None
 external_interface = None
 interface_address = None
+dns_servers = None
+insecure = False
 need_update = False
+ipip_ifl = None
 
+## MQTT
 def createCustomTopic(event_id = DEFAULT_TOPIC):
     data = {}
     data['event_id'] = event_id
@@ -103,12 +111,11 @@ def ifa_cb(message):
     event_id = str(message['jet-event']['event-id']).strip()
     ifd = str(message['jet-event']['attributes']['name']).strip()
     subunit = str(message['jet-event']['attributes']['subunit']).strip()
-    ifl = str(ifd + '.' + subunit).strip()
     family = str(message['jet-event']['attributes']['family']).strip()
     address = str(message['jet-event']['attributes']['local-address']).strip().split('/')[0]
+    ifl = ifd + '.' + subunit
 
     logger.info("Received IFA update: event: {0}, ifl: {1}, family: {2}, address: {3}".format(event_id, ifl, family, address))
-    logger.info("Current ifl: {0}, address: {1}".format(external_interface, interface_address))
     print(event_id == 'KERNEL_EVENT_IFA_ADD')
     print(event_id == 'KERNEL_EVENT_IFA_CHANGE')
     print(family == 'inet6')
@@ -121,17 +128,97 @@ def ifa_cb(message):
             interface_address = address
             need_update = True
 
-def update_configuration():
-    logger.info("update")
+def current_time():
+    """
+    Return current UTC UNIX Time.
+    """
+    return int(datetime.utcnow().timestamp())
+
+def check_time(time):
+    """
+    Return True if specified UTC UNIX time is just now.
+    """
+    return current_time() == time
+
+def random_interval(minimum, maximum):
+    """
+    Return random value between minimum to maximum.
+    """
+    return int((maximum - minimum) * random.random() + minimum)
+
+def set_next_update(interval):
+    """
+    Set next update timing to current time + specified interval(minutes).
+    """
+    global next_update
+    next_update = int(current_time() + interval * 60)
+
+def update_configuration(device):
+    """
+    Try to retrieve provisioning information from provisioning server and apply to configuration if needed.
+    """
+    global provisioned_ttl
+
+    logger.info("Start update")
+    logger.debug("DNS Servers: %s" % ', '.join(dns_servers))
+
+    ps = v6mig.discover_provisioning_server(copy.copy(dns_servers))
+    logger.debug("Provisioning server: %s" % ps)
+
+    if(ps):
+        pd = v6mig.get_provisioning_data(provisioning_server = ps, nameservers = copy.copy(dns_servers), vendorid = VENDOR_ID, product = PRODUCT, version = VERSION, capability = CAPABILITY, insecure = insecure)
+        logger.debug("Provisioning Data: %s" % pd)
+    else:
+        logger.error("Failed to retrieve provisioning server.")
+        # TODO: need to care next update timing for DNS failure.
+        return False
+
+    if(pd):
+        aftr = v6mig.get_aftr_address(pd, copy.copy(dns_servers), multiple = True)
+    else:
+        logger.error("Failed to retrieve provisioning data.")
+        return False
+
+    if(len(aftr)):
+        logger.debug("AFTR(s): %s", str(aftr))
+
+        if len(aftr) > 1:
+            current_aftr = junos.get_current_ipip_destination(device = device, ifl = ipip_ifl)
+            logger.debug("Current configured AFTR: %s", current_aftr)
+            selected_aftr = current_aftr if current_aftr in aftr else aftr[0]
+        else:
+            selected_aftr = aftr[0]
+
+        logger.debug("Selected AFTR: %s" % selected_aftr)
+        config = junos.generate_dslite_configuration(ifl = ipip_ifl, aftr = selected_aftr, source_address = interface_address)
+
+        logger.debug("Generated configuration:\n%s", config)
+    else:
+        logger.error("Failed to retrieve AFTR IP address.")
+        return False
+
+    if(pd['ttl']):
+        provisioned_ttl = int(pd['ttl'])
+
+    junos.update_configuration(device, config)
+    return True
 
 def main():
     global external_interface
     global interface_address
+    global ipip_ifl
+    global dns_servers
+    global insecure
     global need_update
+
+    root_logger = getLogger()
 
     handler = StreamHandler()
     handler.setFormatter(Formatter(LOG_FORMAT))
-    logger.addHandler(handler)
+    root_logger.addHandler(handler)
+
+    getLogger('v6mig_autoconfig.v6mig.junos_utils.main').setLevel('INFO')
+    getLogger('v6mig_autoconfig.v6mig.v6mig.main').setLevel('INFO')
 
     logger.info("Start v6mig_autoconfig")
     parser = argparse.ArgumentParser()
@@ -148,27 +235,64 @@ def main():
         exit(1)
 
     if(args.debug):
-        logger.setLevel(DEBUG)
-
-    device = Device()
-    device.open()
+        logger.setLevel('DEBUG')
+        getLogger('v6mig_autoconfig.v6mig.junos_utils.main').setLevel('DEBUG')
+        getLogger('v6mig_autoconfig.v6mig.v6mig.main').setLevel('DEBUG')
 
     external_interface = args.external_interface
     logger.debug("External interface: %s" % external_interface)
+
+    insecure = True if args.insecure else False
+    ipip_ifl = args.ipip_ifl if args.ipip_ifl else IPIP_IFL
+
+    device = Device()
+    device.open()
 
     interface_address = junos.get_interface_address(device, external_interface)
     if(interface_address == None):
         logger.error("Interface has no IPv6 address!")
         exit(2)
 
+    if(args.area):
+        if(args.area in DNS_SERVERS):
+            dns_servers = DNS_SERVERS[args.area]
+        else:
+            logger.error("Area %s is not found! exit." % args.area)
+            exit(1)
+    else:
+        dns_servers = junos.get_dhcpv6_dns_servers(device, external_interface)
+
+    if(dns_servers is None):
+        logger.error("DNS Server is not set. exit.")
+        exit(2)
+
+    logger.debug("DNS Servers: %s" % ', '.join(dns_servers))
+
     try:
         mqtt_client = open_mqtt()
         subscribe(mqtt_client, createCustomTopic('/junos/events/kernel/interfaces/ifa/#'), ifa_cb)
+
+        need_update = True
         while True:
             time.sleep(0.1)
+            if check_time(next_update):
+                logger.info("Scheduled update will be executed.")
+                need_update = True
+
             if need_update:
                 need_update = False
-                update_configuration()
+                if update_configuration(device):
+                    if(provisioned_ttl):
+                        interval = int(provisioned_ttl / 60)
+                    else:
+                        interval = random_interval(minimum = 60 * 20, maximum = 60 * 24)
+
+                    set_next_update(interval)
+                    logger.info("Update is succeeded or not changed. Wait {0} minutes for next update.".format(str(interval)))
+                else:
+                    interval = random_interval(minimum = 10, maximum = 30)
+                    set_next_update(interval)
+                    logger.info("Update is failed. Wait {0} minutes for next update.".format(str(interval)))
 
     except Exception as ex:
         pass
